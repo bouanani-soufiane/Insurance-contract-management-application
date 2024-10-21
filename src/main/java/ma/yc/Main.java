a11ebda402ac4d5b6f8867bf25dc7cbbb17fb629
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2024-10-21 16:57:37 +0100

    added : Logging configuration using slf4j

--- src/main/java/ma/yc/Main.java
+++ src/main/java/ma/yc/Main.java
@@ -1,7 +1,14 @@
 package ma.yc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main ( String[] args ) {
-        System.out.println("Hello world!");
+        logger.info("This is an INFO message.");
+        logger.warn("This is a WARN message.");
+        logger.error("This is an ERROR message.");
     }
 }
